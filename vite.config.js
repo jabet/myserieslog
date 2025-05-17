a4fdbd86d58134c5dd2b679023d24367ec418499
j_abet <j_abet@me.com>
2025-05-17 20:19:14 +0200

    fix: update base URL in Vite configuration for local development

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://jabet.github.io/myserieslog/',
+  base: '/',
   plugins: [
     react(), 
     tailwindcss()
