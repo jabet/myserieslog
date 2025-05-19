5e91aa54f719ea2a7b16d1c6bf47bdf44b1d79dc
Jesús Abet <j_abet@me.com>
2025-05-19 15:43:13 +0200

    fix: adjust responsive grid layout in CatalogoGrid component

--- src/components/CatalogoGrid.jsx
+++ src/components/CatalogoGrid.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function CatalogoGrid({ catalogo, onEliminar }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4 items-center justify-items-center relative ">
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-6 gap-4 items-center justify-items-center relative ">
       {catalogo.map((item) => (
         <MediaCard
           key={item.id_catalogo}
