f6ae9e316e5b93844834a3b1b8d8c0f288e83984
Jesús Abet <j_abet@me.com>
2025-05-30 15:01:48 +0200

    Refactor code structure for improved readability and maintainability

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,11 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 text-center text-sm text-gray-600 py-4 mt-10 border-t">
       <p className="text-sm text-gray-600">
-        © {new Date().getFullYear()} My Series Log.
+        © {new Date().getFullYear()} My Series Log. Icons by{" "}
+        <a target="_blank" nofollow href="https://icons8.com">
+          Icons8
+        </a>
       </p>
-      
     </footer>
   );
 }
